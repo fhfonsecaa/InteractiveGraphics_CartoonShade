a08f004e7d59580e124cc02764d4ffa7810aebb3
Fabian Fonseca <fhfonsecaa@gmail.com>
2020-04-22 15:31:36 +0200

    First slider implemented

--- main/homework1.js
+++ main/homework1.js
@@ -1,23 +1,25 @@
 "use strict";
 
+var program;
 var canvas;
 var gl;
 
 var numVertices  = 120;
-
 var numChecks = 8;
-
-var program;
 
 var c;
 
-var flag = true;
-
 var pointsArray = [];
 var colorsArray = [];
-var normsArray = [];
+var normalsArray = [];
 
-var thetaLoc;
+var modelViewMatrix, projectionMatrix;
+var theta = 0;
+
+var thetaSlider = document.getElementById("thetaRange");
+thetaSlider.oninput = function() {
+    theta = this.value;
+}
 
 var vertices = [
     vec4(-0.30,-0.580716,0.30,1.0),
@@ -97,15 +99,15 @@ function tria(a, b, c) {
     c=c-1
     pointsArray.push(vertices[a]);
     colorsArray.push(vertexColors[a]);
-    normsArray.push(get_norm(vertices[a],vertices[b],vertices[c]));
+    normalsArray.push(get_norm(vertices[a],vertices[b],vertices[c]));
 
     pointsArray.push(vertices[b]);
     colorsArray.push(vertexColors[a]);
-    normsArray.push(get_norm(vertices[b],vertices[a],vertices[c]));
+    normalsArray.push(get_norm(vertices[b],vertices[a],vertices[c]));
 
     pointsArray.push(vertices[c]);
     colorsArray.push(vertexColors[a]);
-    normsArray.push(get_norm(vertices[c],vertices[a],vertices[b]));
+    normalsArray.push(get_norm(vertices[c],vertices[a],vertices[b]));
 }
 
 function quad(a, b, c, d) {
@@ -113,31 +115,30 @@ function quad(a, b, c, d) {
     tria(a, c, d)
 }
 
-function colorCube(){
+function loadSolid(){
     quad(1,2,4,3);
-    // quad(3,4,8,7);
-    // quad(7,8,6,5);
-    // quad(5,6,2,1);
-    // quad(3,7,5,1);
-    // quad(8,4,2,6);
-    // tria(9,10,11);
-    // tria(11,10,13);
-    // tria(13,10,12);
-    // tria(12,10,9);
-    // quad(11,13,12,9);
-    // tria(14,15,16);
-    // tria(16,15,18);
-    // tria(18,15,17);
-    // tria(17,15,14);
-    // quad(16,18,17,14);
-    // quad(19,20,22,21);
-    // quad(21,22,26,25);
-    // quad(25,26,24,23);
-    // quad(23,24,20,19);
-    // quad(21,25,23,19);
-    // quad(26,22,20,24);
+    quad(3,4,8,7);
+    quad(7,8,6,5);
+    quad(5,6,2,1);
+    quad(3,7,5,1);
+    quad(8,4,2,6);
+    tria(9,10,11);
+    tria(11,10,13);
+    tria(13,10,12);
+    tria(12,10,9);
+    quad(11,13,12,9);
+    tria(14,15,16);
+    tria(16,15,18);
+    tria(18,15,17);
+    tria(17,15,14);
+    quad(16,18,17,14);
+    quad(19,20,22,21);
+    quad(21,22,26,25);
+    quad(25,26,24,23);
+    quad(23,24,20,19);
+    quad(21,25,23,19);
+    quad(26,22,20,24);
 }
-
 
 window.onload = function init() {
 
@@ -151,13 +152,18 @@ window.onload = function init() {
 
     gl.enable(gl.DEPTH_TEST);
 
-    //
     //  Load shaders and initialize attribute buffers
-    //
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    colorCube();
+    loadSolid();
+
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+    var vNormal = gl.getAttribLocation(program, "aNormal");
+    gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
@@ -178,6 +184,15 @@ window.onload = function init() {
 
 var render = function() {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    modelViewMatrix = mat4();
+    // modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], vec3(1, 0, 0)));
+    // modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], vec3(0, 1, 0)));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta, vec3(0, 0, 1)));
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(program,
+            "uModelViewMatrix"), false, flatten(modelViewMatrix));
+
     gl.drawArrays( gl.TRIANGLES, 0, numVertices );
     requestAnimationFrame(render);
 }
